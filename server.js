652410d2669fb63b45e5c24547413c5723dc9ee1
VIVEK YADAV <vivekyadav@VIVEKs-MacBook-Air.local>
2019-09-22 01:38:12 +0530

    add docekr-compose

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Get api is working.. with docker ")
+    res.send("Get api is working.. with docker!! ")
 })
 app.listen(4000, () => {
     console.log(`server running port 4000`)
